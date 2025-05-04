922f71cc14cbd587d942aaa68ed3248d9b37954a
Elian <142859734+Elian7773@users.noreply.github.com>
2025-05-03 23:03:45 -0400

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,23 +1,20 @@
 from django.contrib import admin
-
-# Register your models here.
-from .models import Choice,Question
+from .models import Question, Choice
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 2 # Number of extra blank choice lines to display
+    extra = 2  # Number of extra blank choice lines to display
 
 class QuestionAdmin(admin.ModelAdmin):
-    # The fields to be used in displaying a particular item the Question model for editing
+    # The fields to be used in displaying a particular item in the Question model for editing
     fieldsets = [
         ("Identifying Information", {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date Information", {"fields": ["pub_date"]}),
     ]
     inlines = [ChoiceInline]
 
     # The fields to be used when listing the data in the Question model
     list_display = ("question_text", "pub_date", "was_published_recently")
-    
     list_filter = ["pub_date"]
 
 admin.site.register(Question, QuestionAdmin)
